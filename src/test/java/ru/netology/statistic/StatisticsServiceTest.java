262185700e35ebd6104e7955fbdbc9ed8153f135
vexed <kadirthekid@gmail.com>
2024-01-25 01:32:11 +0300

    changed project

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxSecond() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 80, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 80;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
